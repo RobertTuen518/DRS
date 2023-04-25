835b1efdec6b162cd7e35f81325b1013e1bc349f
Demo Master <70443539+RobertTuen518@users.noreply.github.com>
2023-04-25 16:55:38 +0800

    Update Testing_4.py

--- Testing_4.py
+++ Testing_4.py
@@ -215,7 +215,7 @@ with tab3:
 with tab4:
     # Page 4
     # Retrieve the value of tdee from st.session_state
-    tdee = st.session_state.get("tdee")
+    tdee = st.session_state.tdee
 
     # Create recommend diet button on Page 2
     # Create a button that triggers the diet_recommendation function based on user input
